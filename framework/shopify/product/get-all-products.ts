103716572ce3a4aaf7b6184fb13675daa2777f9c
THÔNG MINH <huynhminhthong1912@gmail.com>
2023-01-02 23:39:13 +0700

    first initial

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,20 @@
+
+import fetchApi from "../utils/fetch-api";
+import getAllProductsQuery from "../utils/queries/get-all-products";
+import { ProductConnection } from "../schema";
+import { type } from "os";
+
+type ReturnType = {
+    products: ProductConnection
+}
+
+const getAllProducts = async (): Promise<any> => {
+    const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
+
+    const products = data.products.edges.map(({ node: product }) => {
+        return product
+    }) ?? [];
+    return products;
+}
+
+export default getAllProducts;
